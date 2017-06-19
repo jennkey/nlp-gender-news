2fa6d5c6780a053fc3230745a17879b312b62ad1
Jennifer Key <jennkey827@gmail.com>
2017-06-19 08:39:49 -0600

    Added code to get male female ratios by each source

--- src/summary_metrics.py
+++ src/summary_metrics.py
@@ -4,6 +4,7 @@ import plotly
 import plotly.graph_objs as go
 from plotly import tools
 import numpy as np
+from source_by_topic_bubbles import bubble_by_source_by_topic
 
 # creates summary metrics for all sources
 
@@ -16,143 +17,96 @@ def read_pickle_df(news_paper):
     df = pd.read_pickle(pickle_to_read)
     return df
 
-def summary_metrics(news_paper):
+def summary_metrics(df, source):
     '''
     Creates summary metrics for each newspaper
     '''
+
+    sources.append(source)
     #overall ratio of male sentences to female sentences
-    ratio_male_to_female_sentences_list.append(float(df['male_sentences'].sum() + df['both_sentences'].sum()) / float(df['female_sentences'].sum() + df['both_sentences'].sum()))
+    male_to_female_ratio = (df['male_sentences'].sum() + df['both_sentences'].sum()) / (df['female_sentences'].sum() + df['both_sentences'].sum())
+    ratio_male_to_female_sentences_list.append(male_to_female_ratio)
 
+    print(source)
     male_articles = len(df[(df['female_sentences'] < df['male_sentences'])])
-    print(male_articles)
+    print('Male articles', male_articles)
     female_articles = len(df[(df['female_sentences'] > df['male_sentences'])])
-    print(female_articles)
-    ratio_male_to_female_articles_list.append(float(male_articles)/float(female_articles))
+    print('Female articles', female_articles)
+    ratio_male_to_female_articles_list.append(male_articles/female_articles)
 
-def bar_chart():
-    # trace1 = go.Bar(
-    #     x=news_papers,
-    #     y=ratio_male_to_female_articles_list,
-    #     name='Articles: Male to Female Ratio'
-    # )
-    #
-    #     plotly.offline.plot(data, filename='bubblechart-size.html')
-    #
-    # trace2 = go.Bar(
-    #     x=news_papers,
-    #     y=ratio_male_to_female_sentences_list,
-    #     name='Sentences: Male to Female Ratio'
-    # )
-    #
-    # data = [trace1, trace2]
-    #layout = go.Layout(
-    #    barmode='group'
-    #)
-    trace0 = go.Bar(
-        x=pretty_names,
-        y=ratio_male_to_female_articles_list,
-        marker=dict(
-            color=[ '#68829E', '#AEBD38', "#598234",]
-            )
-        )
-    trace1 = go.Bar(
-        x=pretty_names,
-        y=ratio_male_to_female_sentences_list,
-        marker=dict(
-            color=[ '#68829E', '#AEBD38', "#598234",],
-            )
-        )
-
-    layout = dict(
-        title='Male to Female Ratios',
-        yaxis1=dict(
-            range=[0,4],
-            showgrid=False,
-            showline=False,
-            showticklabels=True,
-            domain=[0, 0.85],
-        ),
-        yaxis2=dict(
-            range=[0,4],
-            showgrid=False,
-            showline=True,
-            showticklabels=False,
-            linewidth=2,
-            domain=[0, 0.85],
-        ),
-        xaxis1=dict(
-            zeroline=False,
-            showline=False,
-            showticklabels=True,
-            showgrid=True
-        ),
-        xaxis2=dict(
-            zeroline=False,
-            showline=False,
-            showticklabels=True,
-            showgrid=True
-        ),
-        margin=dict(
-            l=100,
-            r=20,
-            t=70,
-            b=70,
-        ),
-
-        paper_bgcolor='rgb(248, 248, 255)',
-        plot_bgcolor='rgb(248, 248, 255)',
-        showlegend=false
-    )
-    annotations = []
-
-    ratio_s = np.round(ratio_male_to_female_sentences_list, decimals=2)
-    ratio_a = np.round(ratio_male_to_female_articles_list, decimals=2)
-    print ratio_s
-    print ratio_a
-
-    # Adding labels
-    for ys, ya, xd in zip(ratio_s, ratio_a, pretty_names):
-        # labeling  bar chart articles
-        annotations.append(dict(xref='x1', yref='y1',
-                                y=ya+.1, x=xd,
-                                text=str(ya),
-                                font=dict(family='Arial', size=12,
-                                          color=[ '#68829E', '#AEBD38', "#598234",]),
-                                showarrow=False))
-        # labeling the bar chart sentences
-        annotations.append(dict(xref='x2', yref='y2',
-                                y=ys+.1, x=xd,
-                                text=str(ys),
-                                font=dict(family='Arial', size=12,
-                                          color=[ '#68829E', '#AEBD38', "#598234",]),
-                                showarrow=False))
-
-    layout['annotations'] = annotations
-    # Creating two subplots
-    fig = tools.make_subplots(rows=1, cols=2,  shared_xaxes=False,
-                              shared_yaxes=False, vertical_spacing=0.001,
-                              subplot_titles=('Articles', 'Sentences'))
-
-    fig.append_trace(trace0, 1, 1)
-    fig.append_trace(trace1, 1, 2)
-
-    fig['layout'].update(layout)
-
-    plotly.offline.plot(fig, filename='ratios.html')
+def aggregate_by_topic(df_unagg):
+    df_agg = df_unagg[['topic_label', 'male_sentences', 'both_sentences', 'female_sentences']].groupby(['topic_label']).sum()
+    df_agg['topic_count'] = df_unagg[['topic_label']].groupby(['topic_label']).size()
+    print(df_agg)
+    return df_agg
 
 
+def fix_source(df):
+    new_source = []
+    for source in df['source']:
+        if source in ('ajc', 'latimes'):
+            new_source.append(source)
+        else:
+            new_source.append(''.join(str(e) for e in source))
+    df['source'] = new_source
 
-
+def count_source(df):
+    source_dict = {}
+    for source in df['source']:
+        source = ''.join(str(e) for e in source)
+        if source in source_dict:
+            source_dict[source] += 1
+        else:
+            source_dict[source] = 1
+    print(source_dict)
 
 
 if __name__ == '__main__':
     news_papers = ['ajc', 'denver_post', 'latimes']
     pretty_names = ["Atlantic Journal Constitution", "The Denver Post", "Los Angeles Times"]
+    # read in pickled dataframe with topics and labels
+    df = read_pickle_df('all')
+
+    # Print out counts of each source
+    fix_source(df)
+    print(pd.value_counts(df['source']))
     ratio_male_to_female_articles_list = []
     ratio_male_to_female_sentences_list = []
-    # read in pickled dataframe with topics and labels
-    for newspaper in news_papers:
-        df = read_pickle_df(newspaper)
-        summary_metrics(newspaper)
 
-    bar_chart()
+    count_source(df)
+
+    # create a dataframe for each source -- change this later but for now need to
+    # do it this way since running out of time and code already expecting this
+    ajc = df[(df['source'] == 'ajc')]
+    latimes = df[(df['source'] == 'latimes')]
+    denver_post = df[(df['source'] == 'Denver Post')]
+
+
+    sources = []
+    ratio_male_to_female_articles_list = []
+    ratio_male_to_female_sentences_list = []
+    summary_metrics(df, 'All')
+    summary_metrics(ajc, 'AJC')
+    summary_metrics(latimes, 'LA Times')
+    summary_metrics(denver_post, 'Denver Post')
+    print(sources)
+    print(ratio_male_to_female_articles_list)
+    print(ratio_male_to_female_sentences_list)
+
+    # create male_percent and female_percent by topic by source
+    df_agg_all = aggregate_by_topic(df)
+    df_agg_ajc = aggregate_by_topic(ajc)
+    df_agg_latimes = aggregate_by_topic(latimes)
+    df_agg_denver_post = aggregate_by_topic(denver_post)
+
+    # append all the aggregated data together just to make it easier to work with
+    df_allsource = df_agg_ajc.append(df_agg_latimes, ignore_index=True)
+    df_allsource = df_allsource.append(df_agg_denver_post, ignore_index=True)
+
+    # create plots for each topic label showing all three sources
+    topic_labels = list(df_all_all['topic_label'])
+    for topic in topic_labels:
+        bubble_by_source_by_topic(topic)
+
+
+    #bar_chart()
